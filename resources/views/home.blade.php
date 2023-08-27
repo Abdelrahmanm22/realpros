daeb43339a19c1ff767ab598ed94eeca031df7a4
Abdelrahmanm22 <abdra1396@gmail.com>
2023-08-27 21:44:43 +0300

    delete files js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,10 @@
 @section('css')
     <!--Internal   Notify -->
     <link href="{{ URL::asset('back/plugins/notify/css/notifIt.css') }}" rel="stylesheet" />
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ URL::asset('back/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('back/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('back/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('page-header')
@@ -172,5 +176,36 @@
         </div><!-- /.container-fluid -->
     </section>
 @endsection
-
+<script src="{{ URL::asset('back/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ URL::asset('back/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+<script>
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "ordering": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+</script>
 @section('js')
